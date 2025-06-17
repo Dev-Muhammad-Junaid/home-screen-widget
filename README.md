2293248b9d0c4f54eea442fcf9a838ca1ce6ad42
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-06-17 13:25:43 +0000

    Updating to latest FlutterFlow output.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# home-screen-widget
-A basic app using homescreen widget for flutterfow
+# HomeWidget
+
+A new Flutter project.
+
+## Getting Started
+
+FlutterFlow projects are built to run on the Flutter _stable_ release.
---
+++ lib/pages/home_page/home_page_model.dart
@@ -0,0 +1,50 @@
+import '/backend/backend.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'home_page_widget.dart' show HomePageWidget;
+import 'package:flutter/material.dart';
+
+class HomePageModel extends FlutterFlowModel<HomePageWidget> {
+  ///  Local state fields for this page.
+
+  int? counter = 0;
+
+  List<HomeDataRecord> homeDataList = [];
+  void addToHomeDataList(HomeDataRecord item) => homeDataList.add(item);
+  void removeFromHomeDataList(HomeDataRecord item) => homeDataList.remove(item);
+  void removeAtIndexFromHomeDataList(int index) => homeDataList.removeAt(index);
+  void insertAtIndexInHomeDataList(int index, HomeDataRecord item) =>
+      homeDataList.insert(index, item);
+  void updateHomeDataListAtIndex(
+          int index, Function(HomeDataRecord) updateFn) =>
+      homeDataList[index] = updateFn(homeDataList[index]);
+
+  ///  State fields for stateful widgets in this page.
+
+  // State field(s) for TextField widget.
+  FocusNode? textFieldFocusNode1;
+  TextEditingController? textController1;
+  String? Function(BuildContext, String?)? textController1Validator;
+  // State field(s) for TextField widget.
+  FocusNode? textFieldFocusNode2;
+  TextEditingController? textController2;
+  String? Function(BuildContext, String?)? textController2Validator;
+  // State field(s) for TextField widget.
+  FocusNode? textFieldFocusNode3;
+  TextEditingController? textController3;
+  String? Function(BuildContext, String?)? textController3Validator;
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {
+    textFieldFocusNode1?.dispose();
+    textController1?.dispose();
+
+    textFieldFocusNode2?.dispose();
+    textController2?.dispose();
+
+    textFieldFocusNode3?.dispose();
+    textController3?.dispose();
+  }
+}
